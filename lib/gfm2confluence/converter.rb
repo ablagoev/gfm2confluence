b9469c269bdb3e3376fd6724a27255f473d52f74
Alexander Dimitrov <alex_or@abv.bg>
2012-08-31 11:48:01 +0300

    Fixes indentation

--- lib/gfm2confluence/converter.rb
+++ lib/gfm2confluence/converter.rb
@@ -7,98 +7,98 @@ require 'redcarpet'
 #TODO: parse <p> tags to paragraphs
 #TODO: parse <iframe> tags
 class Gfm2Confluence::Converter < Redcarpet::Render::Base
-	def block_code(code, language)
-		"{code}\n#{code}{code}\n\n"
-	end
+  def block_code(code, language)
+    "{code}\n#{code}{code}\n\n"
+  end
 
-	def block_quote(quote)
-		"{quote}\n#{quote}{quote}\n"
-	end
+  def block_quote(quote)
+    "{quote}\n#{quote}{quote}\n"
+  end
 
-	def block_html(raw_html)
-		#TODO: try to parse the html body and replce tags where possible
-		raw_html
-	end
+  def block_html(raw_html)
+    #TODO: try to parse the html body and replce tags where possible
+    raw_html
+  end
 
-	def header(text, header_level)
-		"h#{header_level}. #{text}\n"
-	end
+  def header(text, header_level)
+    "h#{header_level}. #{text}\n"
+  end
 
-	def hrule()
-		"----\n"
-	end
+  def hrule()
+    "----\n"
+  end
 
-	def list(contents, list_type)
-		"#{contents}\n"
-	end
+  def list(contents, list_type)
+    "#{contents}\n"
+  end
 
-	def list_item(text, list_type)
-		marker = '-'
-		if list_type.to_s == "ordered"
-			marker = '#'
-		end
-		"#{marker} #{text}"
-	end
+  def list_item(text, list_type)
+    marker = '-'
+    if list_type.to_s == "ordered"
+      marker = '#'
+    end
+    "#{marker} #{text}"
+  end
 
-	def paragraph(text)
-		"#{text}\n\n"
-	end
+  def paragraph(text)
+    "#{text}\n\n"
+  end
 
-	def autolink(link, link_type)
-		"[#{link}]"
-	end
+  def autolink(link, link_type)
+    "[#{link}]"
+  end
 
-	def codespan(code)
-		"{code}#{code}{code}"
-	end
+  def codespan(code)
+    "{code}#{code}{code}"
+  end
 
-	def double_emphasis(text)
-		"*#{text}*"
-	end
+  def double_emphasis(text)
+    "*#{text}*"
+  end
 
-	def emphasis(text)
-		"_#{text}_"
-	end
+  def emphasis(text)
+    "_#{text}_"
+  end
 
-	def image(link, title, alt_text)
-		alt_text.to_s.empty? ? "!#{link}!" : "!#{link}|alt=#{alt_text}!"
-	end
+  def image(link, title, alt_text)
+    alt_text.to_s.empty? ? "!#{link}!" : "!#{link}|alt=#{alt_text}!"
+  end
 
-	def linebreak()
-		"\n"
-	end
+  def linebreak()
+    "\n"
+  end
 
-	def link(link, title, alt_text)
-		alt_text.to_s.empty? ? "[#{link}]" : "[#{alt_text}|#{link}]"
-	end
+  def link(link, title, alt_text)
+    alt_text.to_s.empty? ? "[#{link}]" : "[#{alt_text}|#{link}]"
+  end
 
-	def raw_html(raw_html)
-		#TODO: parse the separate tags along with block_html and make replacements where applicable
-		raw_html
-	end
+  def raw_html(raw_html)
+    #TODO: parse the separate tags along with block_html and make replacements where applicable
+    raw_html
+  end
 
-	def triple_emphasis(text)
-		"*#{text}*"
-	end
+  def triple_emphasis(text)
+    "*#{text}*"
+  end
 
-	def strikethrough(text)
-		"-#{text}-"
-	end
+  def strikethrough(text)
+    "-#{text}-"
+  end
 
-	def superscript(text)
-		"^#{text}^"
-	end
+  def superscript(text)
+    "^#{text}^"
+  end
 
-	def entity(text)
-		"#{text}"
-	end
+  def entity(text)
+    "#{text}"
+  end
 
-	def normal_text(text)
-		escape "#{text}"
-	end
+  def normal_text(text)
+    escape "#{text}"
+  end
 
-	def escape(text)
-		#TODO: Check if anything else needs to be escaped
-		text.gsub(/\{|\}/, '\\\\\0') #This replaces { with \{
-	end
+  def escape(text)
+    #TODO: Check if anything else needs to be escaped
+    text.gsub(/\{|\}/, '\\\\\0') #This replaces { with \{
+  end
 end
--- test/test_gfm2confluence-gem.rb
+++ test/test_gfm2confluence-gem.rb
@@ -2,158 +2,158 @@ require 'rubygems'
 require 'helper'
 
 class TestGfm2Confluence < Test::Unit::TestCase
-	context 'the converter' do
-		should "generate code block" do
-			text = "```js\n//code\n```"
-			expect = "{code}\n//code\n{code}\n\n"
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+  context 'the converter' do
+    should "generate code block" do
+      text = "```js\n//code\n```"
+      expect = "{code}\n//code\n{code}\n\n"
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate block quote" do
-			text = ">Blockquote"
-			expect = "{quote}\nBlockquote\n\n{quote}\n"
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+    should "generate block quote" do
+      text = ">Blockquote"
+      expect = "{quote}\nBlockquote\n\n{quote}\n"
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate block html" do
-			text = "<div>\n<span>Test</span>\n</div>"
-			expect = "<div>\n<span>Test</span>\n</div>\n"
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+    should "generate block html" do
+      text = "<div>\n<span>Test</span>\n</div>"
+      expect = "<div>\n<span>Test</span>\n</div>\n"
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate header" do
-			for i in 1..3 do
-				level = "#" * i
-				text = "#{level} Level#{i}"
-				expect = "h#{i}. Level#{i}\n"
-				assert_equal(expect, Gfm2Confluence.convert(text))
-			end
-		end
+    should "generate header" do
+      for i in 1..3 do
+        level = "#" * i
+        text = "#{level} Level#{i}"
+        expect = "h#{i}. Level#{i}\n"
+        assert_equal(expect, Gfm2Confluence.convert(text))
+      end
+    end
 
-		should "generate hr" do
-			expect = "----\n"
-			assert_equal(expect, Gfm2Confluence.convert('***'))
-			assert_equal(expect, Gfm2Confluence.convert('- - -'))
-			assert_equal(expect, Gfm2Confluence.convert('---------------'))
-		end
+    should "generate hr" do
+      expect = "----\n"
+      assert_equal(expect, Gfm2Confluence.convert('***'))
+      assert_equal(expect, Gfm2Confluence.convert('- - -'))
+      assert_equal(expect, Gfm2Confluence.convert('---------------'))
+    end
 
-		should "generate list" do
-			#Ordered lsits
-			expect = "# Item 1\n# Item 2\n# Item 3\n\n"
-			text = "1. Item 1\n2. Item 2\n3. Item 3"
+    should "generate list" do
+      #Ordered lsits
+      expect = "# Item 1\n# Item 2\n# Item 3\n\n"
+      text = "1. Item 1\n2. Item 2\n3. Item 3"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
+      assert_equal(expect, Gfm2Confluence.convert(text))
 
-			#Unorderd lists
-			expect = "- Item 1\n- Item 2\n- Item 3\n\n"
-			text = "* Item 1\n* Item 2\n* Item 3"
+      #Unorderd lists
+      expect = "- Item 1\n- Item 2\n- Item 3\n\n"
+      text = "* Item 1\n* Item 2\n* Item 3"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-			text = "- Item 1\n- Item 2\n- Item 3"
+      assert_equal(expect, Gfm2Confluence.convert(text))
+      text = "- Item 1\n- Item 2\n- Item 3"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate a paragraph" do
-			expect = "Paragraph text\n\n"
-			text = expect
+    should "generate a paragraph" do
+      expect = "Paragraph text\n\n"
+      text = expect
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "autolink" do
-			expect = "[http://google.com/]\n\n"
-			text = "http://google.com/"
+    should "autolink" do
+      expect = "[http://google.com/]\n\n"
+      text = "http://google.com/"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate code span" do
-			text = "`//code`"
-			expect = "{code}//code{code}\n\n"
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+    should "generate code span" do
+      text = "`//code`"
+      expect = "{code}//code{code}\n\n"
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate double emphasis" do
-			text = "**emphasis**"
-			expect = "*emphasis*\n\n"
+    should "generate double emphasis" do
+      text = "**emphasis**"
+      expect = "*emphasis*\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))			
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))      
+    end
 
-		should "generate emphasis" do
-			text = "*emphasis*"
-			expect = "_emphasis_\n\n"
+    should "generate emphasis" do
+      text = "*emphasis*"
+      expect = "_emphasis_\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate image" do
-			text = "![Alt text](/path/to/img.jpg)"
-			expect = "!/path/to/img.jpg|alt=Alt text!\n\n"
+    should "generate image" do
+      text = "![Alt text](/path/to/img.jpg)"
+      expect = "!/path/to/img.jpg|alt=Alt text!\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
+      assert_equal(expect, Gfm2Confluence.convert(text))
 
-			#Titles are not handled
-			text = "![Alt text](/path/to/img.jpg \"Optional title\")"
-			expect = "!/path/to/img.jpg|alt=Alt text!\n\n"
+      #Titles are not handled
+      text = "![Alt text](/path/to/img.jpg \"Optional title\")"
+      expect = "!/path/to/img.jpg|alt=Alt text!\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate link" do
-			#Link titles are not handled
-			text = "This is [an example](http://example.com/ \"Title\") inline link."
-			expect = "This is [an example|http://example.com/] inline link.\n\n"
+    should "generate link" do
+      #Link titles are not handled
+      text = "This is [an example](http://example.com/ \"Title\") inline link."
+      expect = "This is [an example|http://example.com/] inline link.\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate raw html" do
-			text = "<span>Test</span>"
-			expect = "<span>Test</span>\n\n"
+    should "generate raw html" do
+      text = "<span>Test</span>"
+      expect = "<span>Test</span>\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate triple emphasis" do
-			text = "***empahsis***"
-			expect = "*empahsis*\n\n"
+    should "generate triple emphasis" do
+      text = "***empahsis***"
+      expect = "*empahsis*\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate strikethrough" do
-			text = "~~strike~~"
-			expect = "-strike-\n\n"
+    should "generate strikethrough" do
+      text = "~~strike~~"
+      expect = "-strike-\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate superscript" do
-			text = "^super"
-			expect = "^super^\n\n"
+    should "generate superscript" do
+      text = "^super"
+      expect = "^super^\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate entity" do
-			text = "&amp;"
-			expect = "&amp;\n\n"
+    should "generate entity" do
+      text = "&amp;"
+      expect = "&amp;\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
 
-		should "generate normal text" do
-			text = "normal text"
-			expect = "normal text\n\n"
+    should "generate normal text" do
+      text = "normal text"
+      expect = "normal text\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
+      assert_equal(expect, Gfm2Confluence.convert(text))
 
-			text = "test if { } are escaped properly"
-			expect = "test if \\{ \\} are escaped properly\n\n"
+      text = "test if { } are escaped properly"
+      expect = "test if \\{ \\} are escaped properly\n\n"
 
-			assert_equal(expect, Gfm2Confluence.convert(text))
-		end
-	end
+      assert_equal(expect, Gfm2Confluence.convert(text))
+    end
+  end
 end
